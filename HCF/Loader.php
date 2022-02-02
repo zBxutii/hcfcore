af388dc1c5d8cc5ffb1f2eee846ea33ec795ba78
zBxutii <95422142+zBxutii@users.noreply.github.com>
2022-02-02 00:36:53 -0300

    Update Loader.php

--- HCF/Loader.php
+++ HCF/Loader.php
@@ -1,1 +1,13 @@
+<?php
 
+namespace HCF;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+use HCF\Commands\{CommandLoader};
+
+use HCF\Player\{Player};
+
+class Loader extends PluginBase {
+}
